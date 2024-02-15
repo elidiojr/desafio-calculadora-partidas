75b76f68598c3edadca044298c6ae560f185e9f7
elidiojr <elidiomj@gmail.com>
2024-02-15 01:42:24 -0300

    entrega do segundo desafio

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-let numVitorias = 10
-let numDerrotas = 4
+let numVitorias = 100
+let numDerrotas = 300
 
 let saldoVitorias = fcnSaldoVitorias(numVitorias, numDerrotas)
-
-console.log (saldoVitorias)
+let nivel = fcnNivel(saldoVitorias)
+console.log (`O Herói tem de saldo de ** ${saldoVitorias} ** e está no nível de ** ${nivel}**`)
 
 function fcnSaldoVitorias(vitorias, derrotas) {
    let saldo = vitorias - derrotas
    return saldo
+}
+
+function fcnNivel (saldoVitorias) {
+    let nivel = ""
+    if (saldoVitorias < 10)
+        nivel = "Ferro" 
+    else {
+        if (saldoVitorias >= 11 && saldoVitorias <= 20)
+            nivel = "Bronze"
+        else {
+            if (saldoVitorias >= 21 && saldoVitorias <= 50)
+                nivel = "Prata"
+            else {
+                if (saldoVitorias >= 51 && saldoVitorias <= 80)
+                    nivel = "Ouro"
+                else {
+                    if (saldoVitorias >= 81 && saldoVitorias <= 90)
+                        nivel = "Diamante"
+                    else {
+                        if (saldoVitorias >= 91 && saldoVitorias <= 100)
+                        nivel = "Lendário"
+                    else
+                        nivel = "Imortal"
+                    }
+                }
+            }
+        }
+    }
+    return nivel  
 }
 
