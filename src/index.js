397d759e4bb8cbdf9754494e1f2b7c22f97bc522
elidiojr <elidiomj@gmail.com>
2024-02-15 01:21:30 -0300

    atualizando

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello Word")
+let numVitorias = 10
+let numDerrotas = 4
+
+let saldoVitorias = fcnSaldoVitorias(numVitorias, numDerrotas)
+
+console.log (saldoVitorias)
+
+function fcnSaldoVitorias(vitorias, derrotas) {
+   let saldo = vitorias - derrotas
+   return saldo
+}
+
